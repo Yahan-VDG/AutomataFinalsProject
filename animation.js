906f84d4e2d859339035e9ba7c9d0a3b961ae686
Estelle Marie Dasal <emdasal20@gmail.com>
2023-06-17 00:46:14 +0800

    fixed invalid string continuing

--- animation.js
+++ animation.js
@@ -1,9 +1,10 @@
 const canvas2 = document.getElementById("canvasAnimation");
 const ctx2 = canvas2.getContext("2d");
-const transitionTimer = 500;
+const transitionTimer = 1000;
 
 function animate(lines) {
     let currentFrame = 0;
+    let stopAnimation = false;
     /*for (let i = 0; i < lines.length; i++) {
     lines += lines[i];
     }*/
@@ -11,24 +12,24 @@ function animate(lines) {
     const dfa_1 = {
         state: "q0",
         transitions: {
-            q0: { a: 'invalid', b: 'q1', location: { x: 30, y: 163 } },
-            q1: { a: 'q2', b: 'q2', location: { x: 100, y: 163 } },
-            q2: { a: 'invalid', b: 'q3', location: { x: 170, y: 163 } },
-            q3: { a: 'q4', b: 'q5', location: { x: 240, y: 163 } },
-            q4: { a: 'q4', b: 'q6', location: { x: 310, y: 163 } },
-            q5: { a: 'q7', b: 'q5', location: { x: 310, y: 243 } },
-            q6: { a: 'q7', b: 'q5', location: { x: 380, y: 163 } },
-            q7: { a: 'q9', b: 'q8', location: { x: 450, y: 163 } },
-            q8: { a: 'q11', b: 'q12', location: { x: 520, y: 163 } },
-            q9: { a: 'invalid', b: 'q10', location: { x: 450, y: 243 } },
-            q10: { a: 'q7', b: 'invalid', location: { x: 520, y: 243 } },
-            q11: { a: 'invalid', b: 'q7', location: { x: 520, y: 83 } },
-            q12: { a: 'q13', b: 'q15', location: { x: 590, y: 163 } },
-            q13: { a: 'invalid', b: 'q14', location: { x: 590, y: 83 } },
-            q14: { a: 'q17', b: 'invalid', location: { x: 660, y: 83 } },
-            q15: { a: 'q16', b: 'invalid', location: { x: 590, y: 243 } },
-            q16: { a: 'invalid', b: 'q17', location: { x: 660, y: 243 } },
-            q17: { a: 'q17', b: 'q17', location: { x: 660, y: 163 } },
+            q0: { a: 'invalid', b: 'q1', location: { x: 30, y: 163 }},
+            q1: { a: 'q2', b: 'q2', location: { x: 100, y: 163 }},
+            q2: { a: 'invalid', b: 'q3', location: { x: 170, y: 163 }},
+            q3: { a: 'q4', b: 'q5', location: { x: 240, y: 163 }},
+            q4: { a: 'q4', b: 'q6', location: { x: 310, y: 163 }},
+            q5: { a: 'q7', b: 'q5', location: { x: 310, y: 243 }},
+            q6: { a: 'q7', b: 'q5', location: { x: 380, y: 163 }},
+            q7: { a: 'q9', b: 'q8', location: { x: 450, y: 163 }},
+            q8: { a: 'q11', b: 'q12', location: { x: 520, y: 163 }},
+            q9: { a: 'invalid', b: 'q10', location: { x: 450, y: 243 }},
+            q10: { a: 'q7', b: 'invalid', location: { x: 520, y: 243 }},
+            q11: { a: 'invalid', b: 'q7', location: { x: 520, y: 83 }},
+            q12: { a: 'q13', b: 'q15', location: { x: 590, y: 163 }},
+            q13: { a: 'invalid', b: 'q14', location: { x: 590, y: 83 }},
+            q14: { a: 'q17', b: 'invalid', location: { x: 660, y: 83 }},
+            q15: { a: 'q16', b: 'invalid', location: { x: 590, y: 243 }},
+            q16: { a: 'invalid', b: 'q17', location: { x: 660, y: 243 }},
+            q17: { a: 'q17', b: 'q17', location: { x: 660, y: 163 }},
             invalid: { a: 'invalid', b: 'invalid' }
         },
         changeState(newState) {
@@ -68,7 +69,7 @@ function animate(lines) {
             const currentState = dfa_1.transitions[dfa_1.state];
             testValidity(currentSymbol, currentState);
 
-            if (stateIndex < totalFrames) {
+            if (stateIndex < totalFrames && !stopAnimation) {
                 animateState(stateIndex + 1); // Call the next state transition
             }
         }, transitionTimer);
@@ -91,24 +92,29 @@ function animate(lines) {
             const nextState = currentState.a;
             if (nextState === 'invalid') {
                 invalidHighlight(currentState.location.x, currentState.location.y);
+                stopAnimation = true;
             }
             else {
                 validHighlight(currentState.location.x, currentState.location.y);
                 dfa_1.changeState(nextState);
             }
-        } else if (currentSymbol === 'b') {
+        } 
+        if (currentSymbol === 'b') {
             const nextState = currentState.b;
             if (nextState === 'invalid') {
                 invalidHighlight(currentState.location.x, currentState.location.y);
+                stopAnimation = true;
             } else {
                 validHighlight(currentState.location.x, currentState.location.y);
                 dfa_1.changeState(nextState);
             }
-        } else if (typeof currentSymbol === 'undefined') {
+        } 
+        if (typeof currentSymbol === 'undefined') {
             if (currentState.a === 'q17' && currentState.b === 'q17') {
                 validHighlight(currentState.location.x, currentState.location.y);
             }
         }
+        
         console.log(currentState);
         console.log(currentSymbol);
         currentFrame++;
@@ -168,7 +174,7 @@ function validHighlight(x, y) {
 
     setTimeout(() => {
         ctx2.clearRect(x - 15, y - 15, 30, 30);
-    }, transitionTimer)
+    }, 500)
 }
 
 function invalidHighlight(x, y) {
@@ -180,5 +186,5 @@ function invalidHighlight(x, y) {
 
     setTimeout(() => {
         ctx2.clearRect(x - 15, y - 15, 30, 30);
-    }, transitionTimer)
+    }, 500)
 }
